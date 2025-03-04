f322a9fef65a713fdf045567989b6b73eac5d3b2
shuhei0720 <shuhei_0720@icloud.com>
2025-03-04 21:38:57 +0900

    'カスタムカラーを設定'

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <p className="bg-red-500">こんにちは</p>
+      <p className="bg-purpleLight">hello</p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      green: "#00a497",
+      blue: "#00a3af",
+      heading: "#302833",
+      body: "#2b2b2b",
+      light: "#e7e7eb",
+      pinkLight: "#fdeff2",
+      yellowLight: "#f4dda5",
+      purpleLight: "#dbd0e6",
+      redLight: "#e597b2",
+    },
   },
   plugins: [],
 };
