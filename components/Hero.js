d9bb345b3577e9db0c6ec3e60c2135917491fb06
shuhei0720 <shuhei_0720@icloud.com>
2025-03-05 00:18:50 +0900

    'Heroコンポーネントの大枠を作成'

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section>
+      <div>
+        <div>
+          {/* 左 */}
+          <div>
+            <span>サブタイトル</span>
+            <h1>タイトル</h1>
+            <p>詳細</p>
+            <div>
+              <Link href={""}>資料ダウンロード</Link>
+              <Link href={""}>申し込み</Link>
+            </div>
+          </div>
+
+          {/* 右 */}
+          <div>
+            {/* 上の段 */}
+            <div>
+              <div>
+                <Image />
+              </div>
+              <div>
+                <div>
+                  <div>
+                    <Image />
+                  </div>
+                  <div></div>
+                </div>
+                <div>
+                  <Image />
+                </div>
+              </div>
+            </div>
+
+            {/* 下の段 */}
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <Image />
+                  </div>
+                </div>
+                <div>
+                  <Image />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
