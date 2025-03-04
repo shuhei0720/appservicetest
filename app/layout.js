488a5bf1383b67924243b5441ce03f11fcba4774
shuhei0720 <shuhei_0720@icloud.com>
2025-03-04 21:49:27 +0900

    '必要なコンポーネントを作成'

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { M_PLUS_1_Code } from "next/font/google";
 import "./globals.css";
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
 
 const m_PLUS_1_Code = M_PLUS_1_Code({ subsets: ["latin"] });
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <body className={m_PLUS_1_Code.className}>{children}</body>
+      <body className={m_PLUS_1_Code.className}>
+        <Navigation />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
+import Blog from "@/components/Blog";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import Solutions from "@/components/Solutions";
+import Stats from "@/components/Stats";
+import Testimonials from "@/components/Testimonials";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <p className="bg-purpleLight">hello</p>
+      <Hero />
+      <Stats />
+      <Services />
+      <Solutions />
+      <Testimonials />
+      <Blog />
     </>
   );
 }
