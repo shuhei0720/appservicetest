8cadf9178c0ef5c2541dc00bc02f4033597cd4cb
shuhei0720 <shuhei_0720@icloud.com>
2025-03-04 22:03:13 +0900

    '大枠をコーディング'

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { HiOutlineBars3 } from "react-icons/hi2";
 
 function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <>
+      {/* WEBメニュー */}
+      <header>
+        <div>
+          <div>
+            {/* ロゴ */}
+            <div>
+              <Link href={"/"}>
+                <Image
+                  src={"/assets/logo.png"}
+                  width={90}
+                  height={60}
+                  alt="ロゴ"
+                />
+              </Link>
+            </div>
+            {/* メニュー */}
+            <div>
+              <ul>
+                <li>
+                  <Link href={""}>ホーム、、、</Link>
+                </li>
+              </ul>
+            </div>
+            {/* ボタン */}
+            <div>
+              <Link href={""}>申し込み</Link>
+
+              {/* モバイル用 */}
+              <button>
+                <HiOutlineBars3 />
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* モバイルメニュー */}
+    </>
+  );
 }
 
 export default Navigation;
