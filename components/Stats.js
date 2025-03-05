483ca9e23670d4fce1465517fbde34ee5a35888c
shuhei0720 <shuhei_0720@icloud.com>
2025-03-05 09:00:41 +0900

    'Statsコンポーネントの大枠を作成'

--- components/Stats.js
+++ components/Stats.js
@@ -1,7 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { BiChevronRight } from "react-icons/bi";
 
 function Stats() {
-  return <div>Stats</div>;
+  return (
+    <section>
+      <div>
+        <div>
+          {/* 左側 */}
+          <div>
+            <div>
+              <strong>９９．９％</strong>
+              <span>現役合格率</span>
+            </div>
+          </div>
+
+          {/* 右側 */}
+          <div>
+            <div>
+              <Image src={""} />
+              <h3>タイトル</h3>
+              <p>詳細</p>
+              <Link href={""}>
+                <span>もっと見る</span>
+                <span>
+                  <BiChevronRight />
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Stats;
