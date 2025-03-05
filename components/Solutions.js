5b06b9c132cd1f3296e4e70c88be7d8af909719f
shuhei0720 <shuhei_0720@icloud.com>
2025-03-05 21:59:49 +0900

    'Solutionsコンポーネントのデータを作成して表示'

--- components/Solutions.js
+++ components/Solutions.js
@@ -3,6 +3,38 @@ import Link from "next/link";
 import React from "react";
 import { FaCheck } from "react-icons/fa";
 
+const solutionsContent = {
+  text: {
+    subTitle: "絶対合格を可能にする",
+    Title: "良質な教材・添削",
+    description:
+      "各大学の出題傾向を分析して作成した、ハイグレードな教室オリジナル教材を使用。選び抜かれた良問に取り組むことで、難関大で求められる「思考力」「表現力」を伸ばします",
+    features: [
+      {
+        title: "少人数制だからこそ可能な丁寧な指導",
+      },
+      {
+        title: "選べる学習スタイル",
+      },
+      {
+        title: "講師との距離が近く、質問しやすい",
+      },
+      {
+        title: "静かで集中できる自習室も完備",
+      },
+    ],
+  },
+  images: {
+    img1: "/assets/solution-img-1.jpg",
+    img2: "/assets/solution-img-2.jpg",
+    img3: "/assets/solution-img-3.jpg",
+  },
+  experience: {
+    year: "20年",
+    label: "支援されています",
+  },
+};
+
 function Solutions() {
   return (
     <section id="solutions">
@@ -12,7 +44,12 @@ function Solutions() {
           <div>
             <div>
               <div>
-                <Image />
+                <Image
+                  src={solutionsContent.images.img1}
+                  width={626}
+                  height={640}
+                  alt="img1"
+                />
               </div>
               <div>
                 {/* 下の段 */}
@@ -22,7 +59,12 @@ function Solutions() {
                   </div>
                 </div>
                 <div>
-                  <Image />
+                  <Image
+                    src={solutionsContent.images.img3}
+                    width={626}
+                    height={640}
+                    alt="img3"
+                  />
                 </div>
               </div>
             </div>
@@ -33,12 +75,17 @@ function Solutions() {
                   <div />
                 </div>
                 <div>
-                  <Image />
+                  <Image
+                    src={solutionsContent.images.img2}
+                    width={547}
+                    height={573}
+                    alt="img2"
+                  />
                 </div>
                 <div>
                   <div>
-                    <strong>20年</strong>
-                    <span>支援されています</span>
+                    <strong>{solutionsContent.experience.year}</strong>
+                    <span>{solutionsContent.experience.label}</span>
                   </div>
                 </div>
               </div>
@@ -47,16 +94,18 @@ function Solutions() {
 
           {/* 右 */}
           <div>
-            <span>サブタイトル</span>
-            <h2>タイトル</h2>
-            <p>詳細</p>
+            <span>{solutionsContent.text.subTitle}</span>
+            <h2>{solutionsContent.text.Title}</h2>
+            <p>{solutionsContent.text.description}</p>
             <ul>
-              <li>
-                <span>
-                  <FaCheck />
-                </span>
-                <span>タイトル</span>
-              </li>
+              {solutionsContent.text.features.map((item, index) => (
+                <li key={index}>
+                  <span>
+                    <FaCheck />
+                  </span>
+                  <span>{item.title}</span>
+                </li>
+              ))}
             </ul>
             {/* ボタン */}
             <div>
