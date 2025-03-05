f247de6320c6366ad15f748b565305e3f01d49e7
shuhei0720 <shuhei_0720@icloud.com>
2025-03-05 20:59:33 +0900

    'Servicesコンポーネントの大枠を作成'

--- components/Services.js
+++ components/Services.js
@@ -1,7 +1,34 @@
+import Image from "next/image";
 import React from "react";
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <section id="services">
+      <div>
+        {/* 上の段 */}
+        <div>
+          <span>サブタイトル</span>
+          <h2>タイトル</h2>
+          <p>詳細</p>
+        </div>
+
+        {/* 下の段 */}
+        <div>
+          <div>
+            <div>
+              <span>
+                <Image src={""} />
+              </span>
+            </div>
+            <div>
+              <h3>タイトル</h3>
+              <p>詳細</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Services;
