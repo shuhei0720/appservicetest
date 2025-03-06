b44b0209e1f123d1ec23eb76f4007c4499806711
shuhei0720 <shuhei_0720@icloud.com>
2025-03-06 20:33:44 +0900

    'Testimonialsコンポーネントの大枠を作成'

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,7 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { FaQuoteLeft } from "react-icons/fa";
 
 function Testimonials() {
-  return <div>Testimonials</div>;
+  return (
+    <section id="testimonials">
+      <div>
+        <div>
+          {/* 右 */}
+          <div>
+            <span>subtitle</span>
+            <h2>title</h2>
+            <p>description</p>
+            <div>
+              <Link href={""}>資料ダウンロード</Link>
+            </div>
+          </div>
+
+          {/* 左 */}
+          <div>
+            <div>
+              {/* 2番目だけ弾く */}
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <Image />
+                      <span>
+                        <FaQuoteLeft />
+                      </span>
+                    </div>
+                    <div>
+                      <strong>name</strong>
+                      <span>titlerole</span>
+                    </div>
+                  </div>
+                  <div>
+                    <blockquote>『testimony』</blockquote>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <div />
+                  <div>
+                    <div>
+                      <div>
+                        <Image />
+                        <span>
+                          <FaQuoteLeft />
+                        </span>
+                      </div>
+                      <div>
+                        <strong>name</strong>
+                        <span>titlerole</span>
+                      </div>
+                    </div>
+                    <blockquote>『testimony』</blockquote>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Testimonials;
