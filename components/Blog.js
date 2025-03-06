7383c75de29a4bebd10f75e0800971714b1fe331
shuhei0720 <shuhei_0720@icloud.com>
2025-03-06 21:26:46 +0900

    'Blogコンポーネントの大枠を作成'

--- components/Blog.js
+++ components/Blog.js
@@ -1,7 +1,48 @@
+import Link from "next/link";
 import React from "react";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 function Blog() {
-  return <div>Blog</div>;
+  return (
+    <section id="blog">
+      <div>
+        <div>
+          {/* 左 */}
+          <div>
+            <span>subtitle</span>
+            <h2>title</h2>
+            <p>description</p>
+          </div>
+
+          {/* 右 */}
+          <div>
+            <div>
+              <div>
+                <FaChevronRight />
+              </div>
+              <div>
+                <FaChevronLeft />
+              </div>
+            </div>
+          </div>
+
+          {/* スライド */}
+
+          {/* ボタン */}
+          <div>
+            <Link href={""}>
+              過去のニュースは
+              <strong>こちら</strong>
+              <span>|</span>
+              <span>
+                <FaChevronRight />
+              </span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Blog;
