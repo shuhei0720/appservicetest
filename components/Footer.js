2e25ae00706eeeaadf4d989769be225e47dd43b8
shuhei0720 <shuhei_0720@icloud.com>
2025-03-07 00:11:46 +0900

    'Footerコンポーネントのデータを作成して表示'

--- components/Footer.js
+++ components/Footer.js
@@ -6,6 +6,71 @@ import { FaLocationDot } from "react-icons/fa6";
 import { FaPhoneAlt } from "react-icons/fa";
 import { IoIosLink } from "react-icons/io";
 
+const footerContent = {
+  text: {
+    logo: "/assets/logo.png",
+    description:
+      "成績はもう上がらないとあきらめていませんか？ 成績が上がらないのは能力のせいではありません。今までのやり方を変えるだけで、学力・成績は短期間で飛躍的に伸びます！",
+  },
+  footerLinks: [
+    {
+      heading: "概要",
+      links: [
+        {
+          href: "#",
+          label: "ホーム",
+        },
+        {
+          href: "#",
+          label: "サービス",
+        },
+        {
+          href: "#",
+          label: "ソリューション",
+        },
+        {
+          href: "#",
+          label: "カスタマー",
+        },
+        {
+          href: "#",
+          label: "ブログ・ニュース",
+        },
+      ],
+    },
+    {
+      heading: "その他",
+      links: [
+        {
+          href: "#",
+          label: "ブログ・ニュース",
+        },
+        {
+          href: "#",
+          label: "サポート",
+        },
+        {
+          href: "#",
+          label: "サイトマップ",
+        },
+        {
+          href: "#",
+          label: "プライバシーポリシー",
+        },
+      ],
+    },
+  ],
+  contact: {
+    heading: "連絡先",
+    description: "お気軽にお電話ください",
+    address: {
+      street: "東京都港区芝公園１",
+      phone: "(0120) 123-4567",
+      website: "https://origindoc.com",
+    },
+  },
+};
+
 function Footer() {
   return (
     <footer>
@@ -14,9 +79,14 @@ function Footer() {
           {/* 左 */}
           <div>
             <Link href={"#"}>
-              <Image src={""} />
+              <Image
+                src={footerContent.text.logo}
+                width={90}
+                height={60}
+                alt="logo"
+              />
             </Link>
-            <p>description</p>
+            <p>{footerContent.text.description}</p>
             <Link href={"#"}>
               <span>もっとみる</span>
               <span>
@@ -27,37 +97,41 @@ function Footer() {
           {/* 中央 */}
           <div>
             <div>
-              <div>
-                <h3>linkHeading</h3>
-                <ul>
-                  <li>
-                    <Link href={"#"}>
-                      <span>label</span>
-                      <span>
-                        <BiChevronRight />
-                      </span>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+              {footerContent.footerLinks.map((footerLink, index) => (
+                <div key={index}>
+                  <h3>{footerLink.heading}</h3>
+                  <ul>
+                    {footerLink.links.map((link, index) => (
+                      <li key={index}>
+                        <Link href={"#"}>
+                          <span>{link.label}</span>
+                          <span>
+                            <BiChevronRight />
+                          </span>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
             </div>
           </div>
           {/* 右 */}
           <div>
-            <h3>contactHeading</h3>
-            <p>contactDescription</p>
+            <h3>{footerContent.contact.heading}</h3>
+            <p>{footerContent.contact.description}</p>
             <ul>
               <li>
                 <FaLocationDot />
-                <span>address</span>
+                <span>{footerContent.contact.address.street}</span>
               </li>
               <li>
                 <FaPhoneAlt />
-                <span>phone</span>
+                <span>{footerContent.contact.address.phone}</span>
               </li>
               <li>
                 <IoIosLink />
-                <span>website</span>
+                <span>{footerContent.contact.address.website}</span>
               </li>
             </ul>
           </div>
